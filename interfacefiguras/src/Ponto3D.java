6bbd7961b8a2dbef5abc110ccde5a492a7882296
Beatriz Feitosa <feitosatriz@gmail.com>
2024-08-23 20:31:17 -0300

    implement methods from Objeto3D

--- interfacefiguras/src/Ponto3D.java
+++ interfacefiguras/src/Ponto3D.java
@@ -9,6 +9,30 @@ public class Ponto3D implements Objeto3D {
         this.z = z;
     }
 
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public double getZ() {
+        return z;
+    }
+
+    public void setZ(double z) {
+        this.z = z;
+    }
+
     public void moveZ(double dZ) {
         z += dZ;
     }
--- interfacefiguras/src/SegmentoReta3D.java
+++ interfacefiguras/src/SegmentoReta3D.java
@@ -1,3 +1,62 @@
-public class SegmentoReta3D {
-    
+/**
+ * @author Beatriz Feitosa
+ */
+public class SegmentoReta3D implements Objeto3D {
+    private Ponto3D p0;
+    private Ponto3D p1;
+
+    public SegmentoReta3D(Ponto3D p0, Ponto3D p1) {
+        this.p0 = new Ponto3D(p0.getX(), p0.getY(), p0.getZ());
+        this.p1 = new Ponto3D(p1.getX(), p1.getY(), p1.getZ());;
+    }
+
+    @Override
+    public void moveX(double dX) {
+        p0.moveX(dX);
+        p1.moveX(dX);
+    }
+
+    @Override
+    public void moveY(double dY) {
+        p0.moveY(dY);
+        p1.moveY(dY);
+    }
+
+    @Override
+    public void moveXY(double X, double Y) {
+        p0.moveXY(X, Y);
+        p1.moveXY(X, Y);
+    }
+
+    @Override
+    public void moveZ(double dZ) {
+        p0.moveZ(dZ);
+        p1.moveZ(dZ);
+    }
+
+    @Override
+    public void moveXZ(double dX, double dZ) {
+        p0.moveXZ(dX, dZ);
+        p1.moveXZ(dX, dZ);
+    }
+
+    @Override
+    public void moveYZ(double dY, double dZ) {
+        p0.moveYZ(dY, dZ);
+        p1.moveYZ(dY,dZ);
+    }
+
+    @Override
+    public void moveXYZ(double dX, double dY, double dZ) {
+        p0.moveXYZ(dX, dY, dZ);
+        p1.moveXYZ(dX, dY, dZ);
+    }
+
+    @Override
+    public String toString() {
+        return "SegmentoReta3D{" +
+                "p0=" + p0 +
+                ", p1=" + p1 +
+                '}';
+    }
 }
