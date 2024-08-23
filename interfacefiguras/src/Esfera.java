e4df614058182d81b1a01633892cfe545669613b
Beatriz Feitosa <feitosatriz@gmail.com>
2024-08-23 20:42:21 -0300

    add class Esfera

---
+++ interfacefiguras/src/Esfera.java
@@ -0,0 +1,54 @@
+public class Esfera implements Figura3D {
+    private Ponto3D centro;
+    private double raio;
+
+    public Esfera(Ponto3D centro, double raio) {
+        this.centro = new Ponto3D(centro.getX(), centro.getY(), centro.getZ());
+        this.raio = raio;
+    }
+
+    @Override
+    public double volume() {
+        return 0;
+    }
+
+    @Override
+    public double superficie() {
+        return 0;
+    }
+
+    @Override
+    public void moveX(double dX) {
+        centro.moveX(dX);
+    }
+
+    @Override
+    public void moveY(double dY) {
+        centro.moveY(dY);
+    }
+
+    @Override
+    public void moveXY(double X, double Y) {
+        centro.moveXY(X, Y);
+    }
+
+    @Override
+    public void moveZ(double dZ) {
+        centro.moveZ(dZ);
+    }
+
+    @Override
+    public void moveXZ(double dX, double dZ) {
+        centro.moveXZ(dX, dZ);
+    }
+
+    @Override
+    public void moveYZ(double dY, double dZ) {
+        centro.moveYZ(dY, dZ);
+    }
+
+    @Override
+    public void moveXYZ(double dX, double dY, double dZ) {
+        centro.moveXYZ(dX, dY, dZ);
+    }
+}
---
+++ interfacefiguras/src/Figura3D.java
@@ -0,0 +1,4 @@
+public interface Figura3D extends Objeto3D {
+    double volume();
+    double superficie();
+}
